2ce10ca4ece64fc9ca255128344a9f33fcf84c4b
FranAlcon96 <franplayer96@gmail.com>
2017-11-15 19:36:34 +0100

    Add files via upload

---
+++ ordenadores.sql
@@ -0,0 +1,8 @@
+create table ordenadores( 
+id integer unsigned not null auto_increment, 
+nombre varchar(50) not null, 
+marca varchar(50) not null, 
+precio double not null, 
+pais varchar(50) not null, 
+primary key (id) 
+)auto_increment=1;
